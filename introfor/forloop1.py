f4620780a6b717a640448ce95ec637c5f2217ce1
Diane Richter <diane@bros.net>
2021-03-09 22:59:34 +0000

    loops

---
+++ introfor/forloop1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+for farm in farms:
+    print(f"Farm Name: {farm['name']}")
+    print(f"Grows: ")
+
+    for type in farm['agriculture']:
+        print(f"    {type}")
+
+
